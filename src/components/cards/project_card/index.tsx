c3bb94af9014b589fe13aa1de4b6ed1f434dd5c1
Paulo Henrique Andrade Alves <80326186+PhAlves23@users.noreply.github.com>
2023-09-18 22:25:11 -0300

    feat: project card

---
+++ src/components/cards/project_card/index.tsx
@@ -0,0 +1,56 @@
+import Image from "next/image";
+import Link from "next/link";
+import { FiEye } from "react-icons/fi";
+import { IconType } from "react-icons/lib";
+
+interface ProjectCardProps {
+  id: number;
+  title: string;
+  description: string;
+  image: string;
+  tools: any[] | undefined;
+}
+
+export const ProjectCard = ({
+  id,
+  title,
+  description,
+  image,
+  tools,
+}: ProjectCardProps) => {
+  return (
+    <div className="w-full">
+      <Image
+        src={image ? image : ""}
+        width={384}
+        height={300}
+        alt="Project image"
+        className="rounded-t-2xl w-full"
+      />
+
+      <div className="bg-neutral-800 rounded-b-2xl px-5 py-8">
+        <div className="flex justify-between items-center mb-8">
+          <div>
+            <h3 className="text-2xl font-semibold mb-2">{title}</h3>
+            <p className="text-sm">{description}</p>
+          </div>
+          <Link href={`/projeto_detalhe/${id}`} title="Ver mais">
+            <div className="bg-neutral-700 p-3 lg:p-5 rounded-full">
+              <FiEye size={28} />
+            </div>
+          </Link>
+        </div>
+        <div className="flex items-center gap-2">
+          {tools &&
+            tools.map(({ id, name, path: Path }) => {
+              return (
+                <div key={id}>
+                  <Path size={26} className="ml-2" title={name} />
+                </div>
+              );
+            })}
+        </div>
+      </div>
+    </div>
+  );
+};
